f985294c8d6539cd40afad2ab807f78fd83acd44
Powercat-code <55106022+Powercat-code@users.noreply.github.com>
2019-11-05 09:46:00 -0700

    Initial commit

---
+++ Web migration IIS7.ps1
@@ -0,0 +1,318 @@
+﻿cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="activeadvantage.acbrewards.com" -dest:package=c:\rs-pkgs\activeadvantage.acbrewards.com.zip',encryptPassword=1234' 
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="alarisrewards.com" -dest:package=c:\rs-pkgs\alarisrewards.com.zip',encryptPassword=1234' 
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="AmeriGasRebate.com" -dest:package=c:\rs-pkgs\AmeriGasRebate.com.zip',encryptPassword=1234' 
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="amerigasrebatecard.com" -dest:package=c:\rs-pkgs\amerigasrebatecard.com.zip',encryptPassword=1234' 
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="approvalguardgift.acbrewards.com" -dest:package=c:\rs-pkgs\approvalguardgift.acbrewards.com.zip',encryptPassword=1234' 
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="aquabotrewards.com" -dest:package=c:\rs-pkgs\aquabotrewards.com.zip',encryptPassword=1234' 
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="ArmoredAutoRebates.com" -dest:package=c:\rs-pkgs\ArmoredAutoRebates.com.zip',encryptPassword=1234' 
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="atdpromotions.acbrewards.com" -dest:package=c:\rs-pkgs\atdpromotions.acbrewards.com.zip',encryptPassword=1234' 
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="atomosflamepromo.com" -dest:package=c:\rs-pkgs\atomosflamepromo.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="autodealership.rewards.wernerco.ca" -dest:package=c:\rs-pkgs\autodealership.rewards.wernerco.ca.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="autodealership.rewards.wernerco.com" -dest:package=c:\rs-pkgs\autodealership.rewards.wernerco.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="autoliterebates.com" -dest:package=c:\rs-pkgs\autoliterebates.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="BEDSLIDEDEALER.com" -dest:package=c:\rs-pkgs\BEDSLIDEDEALER.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="campbellhausfeldincentives.com" -dest:package=c:\rs-pkgs\campbellhausfeldincentives.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="cashinwithtrelleborg.com" -dest:package=c:\rs-pkgs\cashinwithtrelleborg.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="castroldistributorrewards.com" -dest:package=c:\rs-pkgs\castroldistributorrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="channelpartnerperks.acbrewards.com" -dest:package=c:\rs-pkgs\channelpartnerperks.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="clopay.acbrewards.com" -dest:package=c:\rs-pkgs\clopay.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="clubtsc.acbincentives.com" -dest:package=c:\rs-pkgs\clubtsc.acbincentives.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="customer.rewards.wernerco.com" -dest:package=c:\rs-pkgs\customer.rewards.wernerco.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="distributor.rewards.wernerco.ca" -dest:package=c:\rs-pkgs\distributor.rewards.wernerco.ca.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="distributor.rewards.wernerco.com" -dest:package=c:\rs-pkgs\distributor.rewards.wernerco.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="employee.rewards.wernerco.com" -dest:package=c:\rs-pkgs\employee.rewards.wernerco.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="fr.vredesteinrewards.com" -dest:package=c:\rs-pkgs\fr.vredesteinrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="fujitsuchannel.acbrewards.com" -dest:package=c:\rs-pkgs\fujitsuchannel.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="gcpcontractorrewards.com" -dest:package=c:\rs-pkgs\gcpcontractorrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="geico.acbincentives.com" -dest:package=c:\rs-pkgs\geico.acbincentives.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="gobigerewards.com" -dest:package=c:\rs-pkgs\gobigerewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="graceiws35th.acbrewards.com" -dest:package=c:\rs-pkgs\graceiws35th.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="gracerewards.acbrewards.com" -dest:package=c:\rs-pkgs\gracerewards.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="hires.rebate.acbincentives.com" -dest:package=c:\rs-pkgs\hires.rebate.acbincentives.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="ingersollrandoffers.acbrewards.com" -dest:package=c:\rs-pkgs\ingersollrandoffers.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="internal.kraftoperatorprogram.com" -dest:package=c:\rs-pkgs\internal.kraftoperatorprogram.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="kraftoperatorprogram.com" -dest:package=c:\rs-pkgs\kraftoperatorprogram.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="leerrewards.com" -dest:package=c:\rs-pkgs\leerrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="lereward.com" -dest:package=c:\rs-pkgs\lereward.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="lerewards.com" -dest:package=c:\rs-pkgs\lerewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="lifecorerewards.com" -dest:package=c:\rs-pkgs\lifecorerewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="malarkey.acbrewards.com" -dest:package=c:\rs-pkgs\malarkey.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="mellanoxoffers.com" -dest:package=c:\rs-pkgs\mellanoxoffers.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="MichelinRebate.acbrewards.com" -dest:package=c:\rs-pkgs\MichelinRebate.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="MicrovisionRebate.acbcoop.com" -dest:package=c:\rs-pkgs\MicrovisionRebate.acbcoop.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="modespiffs.com" -dest:package=c:\rs-pkgs\modespiffs.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="Neatoroboticsrewards.acbrewards.com" -dest:package=c:\rs-pkgs\Neatoroboticsrewards.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="nucrebate.com" -dest:package=c:\rs-pkgs\nucrebate.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="oilchangeincentive.acbrewards.com" -dest:package=c:\rs-pkgs\oilchangeincentive.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="otrspiff.titantirerewards.com" -dest:package=c:\rs-pkgs\otrspiff.titantirerewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="paceedwardsrewards.com" -dest:package=c:\rs-pkgs\paceedwardsrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="profotorewards.com" -dest:package=c:\rs-pkgs\profotorewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="propanerebate.com" -dest:package=c:\rs-pkgs\propanerebate.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="propanerebatecard.com" -dest:package=c:\rs-pkgs\propanerebatecard.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="propanerebatecard.com" -dest:package=c:\rs-pkgs\propanerebatecard.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="prorewards.acbrewards.com" -dest:package=c:\rs-pkgs\prorewards.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="PRWrewards.PRW-USA.com" -dest:package=c:\rs-pkgs\PRWrewards.PRW-USA.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="rebaterewardoffer.com" -dest:package=c:\rs-pkgs\rebaterewardoffer.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="rebates.acbincentives.com" -dest:package=c:\rs-pkgs\rebates.acbincentives.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="riskadvantageprogram.acbincentives.com" -dest:package=c:\rs-pkgs\riskadvantageprogram.acbincentives.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="rubbermaidrebateoffers.com" -dest:package=c:\rs-pkgs\rubbermaidrebateoffers.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="salesagency.rewards.wernerco.com" -dest:package=c:\rs-pkgs\salesagency.rewards.wernerco.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="scansnappromotions.acbrewards.com" -dest:package=c:\rs-pkgs\scansnappromotions.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="sigrewards.com" -dest:package=c:\rs-pkgs\sigrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="sigsauerrebates.com" -dest:package=c:\rs-pkgs\sigsauerrebates.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="sigsauerrewards.com" -dest:package=c:\rs-pkgs\sigsauerrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="sony.rebates.acbincentives.com" -dest:package=c:\rs-pkgs\sony.rebates.acbincentives.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="sonyrebates.acbincentives.com" -dest:package=c:\rs-pkgs\sonyrebates.acbincentives.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="spiff.atdpromotions.acbrewards.com" -dest:package=c:\rs-pkgs\spiff.atdpromotions.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="spiff.titantirerewards.com" -dest:package=c:\rs-pkgs\spiff.titantirerewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="spiff.vredesteinrewards.com" -dest:package=c:\rs-pkgs\spiff.vredesteinrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="standbyreferafriend.com" -dest:package=c:\rs-pkgs\standbyreferafriend.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="TireProsRebate.acbrewards.com" -dest:package=c:\rs-pkgs\TireProsRebate.acbrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="tireprosrebate.com" -dest:package=c:\rs-pkgs\tireprosrebate.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="titandealer.acbincentives.com" -dest:package=c:\rs-pkgs\titandealer.acbincentives.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="TitanTireRewards.com" -dest:package=c:\rs-pkgs\TitanTireRewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="traegerrewards.com" -dest:package=c:\rs-pkgs\traegerrewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="trelleborg-ag-rewards.com" -dest:package=c:\rs-pkgs\trelleborg-ag-rewards.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="trustamerigasrebate.com" -dest:package=c:\rs-pkgs\trustamerigasrebate.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="trxtrainingoffers.com" -dest:package=c:\rs-pkgs\trxtrainingoffers.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="Upgrade2hdr.atomosrebate.com" -dest:package=c:\rs-pkgs\Upgrade2hdr.atomosrebate.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="weilerrebates.com" -dest:package=c:\rs-pkgs\weilerrebates.com.zip',encryptPassword=1234'
+
+
+cd "C:\Program Files\IIS\Microsoft Web Deploy V3"
+.\msdeploy -verb:sync -source:apphostconfig="weilerrewards.com" -dest:package=c:\rs-pkgs\weilerrewards.com.zip',encryptPassword=1234'
